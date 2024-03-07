2de6904a2ec0fdc52f2827729142555cca3b84f3
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-03-07 09:51:40 +0100

    commit

--- artist.js
+++ artist.js
@@ -2,7 +2,9 @@ const url = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 
 // const artistId = new URLSearchParams(window.location.search).get("id");
 
-const artistId = 555;
+const artistId = Math.floor(Math.random);
+
+const sinistra = document.querySelector(".sinistra");
 
 const artFetch = (artistId) => {
   fetch(url + artistId, {
@@ -24,6 +26,29 @@ const artFetch = (artistId) => {
     .catch((errore) => console.log(errore));
 };
 
+const sxFetch = (artistId) => {
+  fetch(url + artistId, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Errore!");
+      }
+    })
+    .then((artist) => {
+      console.log(artist);
+      document.querySelector(".nomeArtista").innerText = artist.name;
+      const heroDiv = document.querySelector(".heroDiv");
+      heroDiv.style.backgroundImage = `url(${artist.picture_xl})`;
+    })
+    .catch((errore) => console.log(errore));
+};
+
 window.onload = () => {
   artFetch(artistId);
+  sinistra.addEventListener("click", () => {
+    sxFetch();
+  });
 };
